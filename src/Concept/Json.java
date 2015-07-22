83d1e65de8a0e338d0e2be5583bdb90a62dfc237
drowsy810301 <lbj39919538@gmail.com>
2015-07-22 22:59:33 +0800

    use arraylist

--- src/Concept/Json.java
+++ src/Concept/Json.java
@@ -1,25 +1,27 @@
 package Concept;
 
+import java.util.ArrayList;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Json {
 	
-	public static int numFound;	
-	public static String[] context;
-	public static String[] dataset;
-	public static String[] end;
+	private static int numFound;	
+	private static ArrayList<String> context = new ArrayList<String>();
+	private static ArrayList<String> dataset = new ArrayList<String>();
+	private static ArrayList<String> end = new ArrayList<String>();
 	//public static
-	public static String[] id;
-	public static String[] license;
-	public static String[] rel;
-	public static String[] source_uri;
+	private static ArrayList<String> id = new ArrayList<String>();
+	private static ArrayList<String> license = new ArrayList<String>();
+	private static ArrayList<String> rel = new ArrayList<String>();
+	private static ArrayList<String> source_uri = new ArrayList<String>();
 	//public static
-	public static String[] start;
-	public static String[] surfaceText;
-	public static String[] uri;
-	public static double[] weight;
+	private static ArrayList<String> start = new ArrayList<String>();
+	private static ArrayList<String> surfaceText = new ArrayList<String>();
+	private static ArrayList<String> uri = new ArrayList<String>();
+	private static ArrayList<Double> weight = new ArrayList<Double>();;
 	
 	public static void parse(String str){
 		JSONObject json = null;
@@ -31,33 +33,20 @@ public class Json {
 		try {
 			JSONArray jsonAr = json.getJSONArray("edges");
 			//System.out.println(jsonOb.length());
-			/*** ***/
-			context = new String[jsonAr.length()];
-			dataset = new String[jsonAr.length()];
-			end = new String[jsonAr.length()];
-			id = new String[jsonAr.length()];
-			license = new String[jsonAr.length()];
-			rel = new String[jsonAr.length()];
-			source_uri = new String[jsonAr.length()];
-			start = new String[jsonAr.length()];
-			surfaceText = new String[jsonAr.length()];
-			uri = new String[jsonAr.length()];
-			weight = new double[jsonAr.length()];
-			
-			for(int i=0; i<jsonAr.length(); i++){
-				
+			/*** ***/						
+			for(int i=0; i<jsonAr.length(); i++){				
 				JSONObject jsonItem = jsonAr.getJSONObject(i);
-				context[i] = jsonItem.getString("context");
-				dataset[i] = jsonItem.getString("dataset");
-				end[i] = jsonItem.getString("end");
-				id[i] = jsonItem.getString("id");
-				license[i] = jsonItem.getString("license");
-				rel[i] = jsonItem.getString("rel");
-				source_uri[i] = jsonItem.getString("source_uri");
-				start[i] = jsonItem.getString("start");
-				surfaceText[i] = jsonItem.getString("surfaceText");
-				uri[i] = jsonItem.getString("uri");
-				weight[i] = jsonItem.getDouble("weight");				
+				context.add(jsonItem.getString("context"));
+				dataset.add(jsonItem.getString("dataset"));
+				end.add(jsonItem.getString("end"));
+				id.add(jsonItem.getString("id"));
+				license.add(jsonItem.getString("license"));
+				rel.add(jsonItem.getString("rel"));
+				source_uri.add(jsonItem.getString("source_uri"));
+				start.add(jsonItem.getString("start"));
+				surfaceText.add(jsonItem.getString("surfaceText"));
+				uri.add(jsonItem.getString("uri"));
+				weight.add(jsonItem.getDouble("weight"));				
 				
 			}		
 			numFound = json.getInt("numFound");			
@@ -66,46 +55,46 @@ public class Json {
 		}
 	}
 	
-	public static String[] get_context(){
+	public static ArrayList<String> get_context(){
 		return context;		
 	}
-	public static String[] get_dataset(){
+	public static ArrayList<String> get_dataset(){
 		return dataset;		
 	}
-	public static String[] get_end(){
-		for(int i=0; i<numFound; i++){
-			end[i] = new String(end[i].substring(6));
-		}
+	public static ArrayList<String> get_end(){
+		for(int i=0; i<numFound; i++)
+			end.set(i, end.get(i).substring(6));
+		
 		return end;		
 	}
-	public static String[] get_id(){
+	public static ArrayList<String> get_id(){
 		return id;		
 	}
-	public static String[] get_license(){
+	public static ArrayList<String> get_license(){
 		return license;		
 	}
-	public static String[] get_rel(){
-		for(int i=0; i<numFound; i++){
-			rel[i] = new String(rel[i].substring(3));
-		}
+	public static ArrayList<String> get_rel(){
+		for(int i=0; i<numFound; i++)
+			rel.set(i, rel.get(i).substring(3));
+		
 		return rel;		
 	}
-	public static String[] get_source_uri(){
+	public static ArrayList<String> get_source_uri(){
 		return source_uri;		
 	}
-	public static String[] get_start(){
+	public static ArrayList<String> get_start(){
 		for(int i=0; i<numFound; i++){
-			start[i] = new String(start[i].substring(6));
+			start.set(i, start.get(i).substring(6));
 		}
 		return start;			
 	}
-	public static String[] get_surfaceText(){
+	public static ArrayList<String> get_surfaceText(){
 		return surfaceText;		
 	}
-	public static String[] get_uri(){
+	public static ArrayList<String> get_uri(){
 		return uri;		
 	}
-	public static double[] get_weight(){
+	public static ArrayList<Double> get_weight(){
 		return weight;		
 	}
 	public static int get_number(){
--- src/Concept/Main.java
+++ src/Concept/Main.java
@@ -14,25 +14,32 @@ public class Main {
 		String str = new String();
 		String tone = new String();
 		ArrayList<String> rhyme =  new ArrayList<String>();
-		String concept = new String("著");
+		String concept = new String("人");
 		Scanner input = new Scanner(System.in);		 
 		//topic = input.next();
 		//System.out.println(topic);
+		ArrayList<Double> start = new ArrayList<Double>();
 		
-		/*
 		try {
-			//url = new URL("http://conceptnet5.media.mit.edu/data/5.3/c/zh/" + Crawler.encode(concept));
-			url = new URL("http://www.chinese-artists.net/poem/?query=" + Crawler.encode(concept));
+			url = new URL("http://conceptnet5.media.mit.edu/data/5.3/c/zh/" + Crawler.encode(concept));
+			//url = new URL("http://www.chinese-artists.net/poem/?query=" + Crawler.encode(concept));
 		} catch (MalformedURLException e) {			
 			e.printStackTrace();
 		}
 		str = Crawler.crawl(url);
-		System.out.println(str);
-		*/
+		Json.parse(str);
+		start = Json.get_weight();
+		for(int i=0; i<Json.get_number(); i++){
+			System.out.println(start.get(i));
+		}
+		//System.out.println(str);
+		
+		/*
 		rhyme = Tone.get_rhyme(concept);
 		for(int i=0; i<rhyme.size(); i++)
 			System.out.println(rhyme.get(i));
-		//Json.parse(str);		
+		*/
+		
 	}	
 	
 }
